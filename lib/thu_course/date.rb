85ac3cde7b99ffc50fad232c74a86d5c91ac9e01
Unayung Chen <unayung@gmail.com>
2017-10-03 18:57:05 +0800

    fix period and location issue

--- lib/thu_course/date.rb
+++ lib/thu_course/date.rb
@@ -16,26 +16,26 @@ def date_change(datas)
     if data.size == 1 && (data =~ /\p{han}/) != nil
       day = data
       case day
-      when '一'
-        day_num = 1
-      when '二'
-        day_num = 2
-      when '三'
-        day_num = 3
-      when '四'
-        day_num = 4
-      when '五'
-        day_num = 5
-      when '六'
-        day_num = 6
-	  when '日'
-		day_num = 7
+        when '一'
+          day_num = 1
+        when '二'
+          day_num = 2
+        when '三'
+          day_num = 3
+        when '四'
+          day_num = 4
+        when '五'
+          day_num = 5
+        when '六'
+          day_num = 6
+    	  when '日'
+    		  day_num = 7
       end
 
       hash << { day: day_num, local: local, time: time.reverse }
       time = []
     elsif data != ''
-      if data.size >= 3 || data.include?('館')
+      if data.size >= 3 || data.include?('館') || data.include?('系圖')
         local = data
       else
         time << data
